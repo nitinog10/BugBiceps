0daa2d4f0b653a9e2dd06affbcdc9a736da3028f
Nitin Kumar Mishra <nitiniszod10@gmail.com>
2026-07-26 22:47:34 +0530

    abc

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    open: false
+    open: false,
+    fs: {
+      allow: ['.', 'C:/Users/DELL/.gemini/antigravity-ide/brain/0ec0b2b1-9104-4782-afc5-61a448290d11']
+    }
   }
 })
